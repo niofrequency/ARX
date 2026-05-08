11e428fcca4c6c6f53d0c57da2e7afe290b8cfa7
niofrequency <124836368+niofrequency@users.noreply.github.com>
2026-05-08 19:10:11 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2950,7 +2950,7 @@ export default function App() {
                     </div>
                   ))
                 )}
-              </div>
+              </div> 
             </motion.div>
           </>
         )}
